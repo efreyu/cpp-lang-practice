3ac0117ffd841e5afada2f681c120260fe7bafcc
efreyu <faustkeller45@gmail.com>
2019-06-17 00:44:23 +0300

    master | lesson1

---
+++ chapter1/lesson1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by efreyu on 6/17/2019.
+//
+#include <iostream>
+
+int main() {
+    int first, second, sum;
+    std::cout << "Enter two numbers: ";
+    std::cin >> first >> second;
+    sum = first + second;
+    std::cout << "Your numbers: " << sum << std::endl;
+    return 0;
+}
