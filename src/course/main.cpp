20c62b5affcfdf57f3049ec6e7ad3fa28509a621
Artur Barilo <faustkeller45@gmail.com>
2019-07-23 11:41:13 +0300

    lesson1 | add cmake to project

---
+++ src/course/main.cpp
@@ -0,0 +1,10 @@
+#include "program.h"
+
+#define IT_WORKS "It works!"
+
+int main() {
+    const int SUCCESS = 0;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << IT_WORKS << std::endl;
+    return SUCCESS;
+}
